8127408b6dba2ddbeed3842c397c29abf9b61116
Tran Tu <65528999+GitHubCodeForLife@users.noreply.github.com>
2022-06-14 22:05:05 +0700

    payment

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,32 @@
-module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: ['react', 'import', 'prettier'],
-  rules: {
-    'no-console': 'warn',
-    'import/first': 'error',
-    'react/prop-types': 0,
-    'linebreak-style': ['error', 'unix'],
-    'arrow-body-style': 'off',
-    'prefer-arrow-callback': 'off',
-    'prettier/prettier': [
-      'error',
-      {
-        singleQuote: true,
-        semi: true,
-        jsxSingleQuote: true,
-      },
-    ],
-  },
-};
+// module.exports = {
+//   env: {
+//     browser: true,
+//     es2021: true,
+//     node: true,
+//   },
+//   extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+//   parserOptions: {
+//     ecmaFeatures: {
+//       jsx: true,
+//     },
+//     ecmaVersion: 12,
+//     sourceType: 'module',
+//   },
+//   plugins: ['react', 'import', 'prettier'],
+//   rules: {
+//     'no-console': 'warn',
+//     'import/first': 'error',
+//     'react/prop-types': 0,
+//     'linebreak-style': ['error', 'unix'],
+//     'arrow-body-style': 'off',
+//     'prefer-arrow-callback': 'off',
+//     'prettier/prettier': [
+//       'error',
+//       {
+//         singleQuote: true,
+//         semi: true,
+//         jsxSingleQuote: true,
+//       },
+//     ],
+//   },
+// };
